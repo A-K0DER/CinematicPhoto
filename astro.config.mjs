82928369624dccfc76f26d2a0126f6721f1c82bc
“akmrsool” <a.kumar@mrsool.co>
2026-08-11 15:03:56 +0530

    fix: update sitemap integration to filter out editor page

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,11 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://cinematicphoto.com',
-	integrations: [sitemap()],
+	integrations: [
+		sitemap({
+			filter: (page) => page !== 'https://cinematicphoto.com/editor/',
+		}),
+	],
 	vite: {
 		plugins: [tailwindcss()],
 	},
